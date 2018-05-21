8c8360c353a421992a00a79b9f73bb82ce28b785
LampicJ15 <jan.lampic13@gmail.com>
2018-05-21 12:09:46 +0200

    loss_function
    Still needs a little discussion

---
+++ loss_function.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+
+def jaccard_loss(P,T):
+    """jaccard_loss function recieves 2 arguments P-predictions, T- true values
+     and returns a value 1 - Jaccard index"""
+
+    #power stand for |.| (moc mnozice)
+    #PT stands for the element wise product of matrix P and T
+    powerPT = tf.reduce_sum(tf.multiply(P,T))
+    powerP = tf.reduce_sum(P)
+    powerT = tf.reduce_sum(T)
+
+    return 1 - powerPT/(powerP + powerT - powerPT)
+
+
+#####################################################3
+#### test
+##Dice in Jaccard
+
+def dice(a,b):
+    sumAB = np.sum(a*b)
+    sumA = np.sum(a)
+    sumB = np.sum(b)
+    return 2*sumAB/(sumA+sumB)
+
+def jaccard(a,b):
+    sumAB = np.sum(a * b) #to naredi produkt po elemtih
+    sumA = np.sum(a)
+    sumB = np.sum(b)
+    return (sumAB)/(sumA + sumB - sumAB)
+
+def jaccard2(P, T):
+    #fukncija izgube kot je v članku
+    normPT = np.linalg.norm(P * T)
+    normP = np.linalg.norm(P)
+    normT = np.linalg.norm(T)
+
+    return normPT/(normP**2 + normT**2 - normPT)
+
